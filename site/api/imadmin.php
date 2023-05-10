2cec12ec9c32d7d4d727318d855ab22adaa0cc75
bguyon <brewal.guyon@univ-rennes1.fr>
2023-05-10 14:32:07 +0200

    ajout api (fichier imadmin.php qui va serir aux tests) + correction des autres fichiers php

---
+++ site/api/imadmin.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+$_SESSION["statut"] = "administrateur";
+$_SESSION["pseudo"] = "admini";
+
+
+?>
--- site/suppression.php
+++ site/suppression.php
@@ -18,15 +18,15 @@ session_start();
 <body>
     <?php
         if (!empty($_SESSION) && isset($_SESSION["pseudo"])){
-            if (isset($_SESSION("statut") && $_SESSION["statut"] === "administrateur")){
-                echo "<h1>Welcome admin</h1>"
+            if (isset($_SESSION["statut"]) && $_SESSION["statut"] === "administrateur"){
+                echo "<h1>Welcome admin</h1>";
             }
             else{
-                echo '<script>$window.location = index.php</script>'
+                echo '<script>window.location = "index.php"</script>';
             }
         }
         else{
-            echo '<script>$window.location = connection.php</script>'
+            echo '<script>window.location = "connection.php"</script>';
         }
     ?>
 </body>
