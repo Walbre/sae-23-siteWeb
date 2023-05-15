91d7bbdc6537f0fbc8a2d9a1c5bba4774cc09006
OldSwade <133116353+OldSwade@users.noreply.github.com>
2023-05-15 15:00:22 +0200

    connexion.php (version 1.2)
    Error fixes (page unfinished)

--- site/connexion.php
+++ site/connexion.php
@@ -13,20 +13,34 @@ require "fonctions.php";
     <body>
 <?php
 if (!empty($_GET) && isset($_GET["action"]) && $_GET["action"]=="logout") {
-				echo "supression";
 				session_destroy();
 				$_SESSION=array();
 			}			
 
-if (!empty($_POST) && isset($_POST['login']) && isset($_POST['pass']))	{		
-	if ( authentification($_POST["login"],$_POST["pass"]) )		{					
+if (!empty($_POST) && isset($_POST['login']) && isset($_POST['pass']) && validate($_POST['pass']))	{	
+	if ((validate($_POST['pass']))) {
+
+		echo "mdp pas valide";
+	}
+
+	else{
+		echo "mdp valide";
+	}
+
+
+	/*if ( authentification($_POST["login"],$_POST["pass"]) )		{					
 		$_SESSION["login"]=$_POST["login"];
-			if ($_POST["login"]=="admin") {
+			if ($_POST["login"]=="admin" && $_POST["pass"]=="Lannion!")  {
 				$_SESSION["statut"]="administrateur";
+				echo "<p>C'est bon</p>";
 			}
 			else {
 			$_SESSION["statut"]="utilisateur";
 			}
+	}
+*/
+
+
 ?>	
 	    
 <h1>Accueil depuis la page initiale</h1>
@@ -35,21 +49,22 @@ if (!empty($_POST) && isset($_POST['login']) && isset($_POST['pass']))	{
 	    
 <?php			
 				
-	}
+	
 }
 			
 
 if (empty($_SESSION)){
+
+
+
 ?>
 	<form id="cnx" method="post" action="connexion.php">
-		<p><label for="login">Login :</label><input type="text" id="login" name="login" /></p>
-		<p><label for="pass">Mot de Passe :</label><input type="password" id="pass" name="pass" /></p>
+		<p><label for="login">Login : </label><input type="text" id="login" name="login" /></p>
+		<p><label for="pass">Mot de Passe : </label><input type="password" id="pass" name="pass" /></p>
 		<p><input type="submit" id="submit" name="submit" value="Connexion" /></p>
 	</form>
 <?php
 }
 ?>	    
-
-	    <p>test</p>
 
     </body>
