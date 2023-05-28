9b60a9e064d6ce88efb1d7b718c55f2ea15633e0
CHRISTOPHE GUYON <christopheguyon@Mac-mini-de-CHRISTOPHE.local>
2023-05-28 14:04:14 +0200

    page suppression (sans supprimer)

--- site/suppression.php
+++ site/suppression.php
@@ -19,8 +19,30 @@ require "fonctions.php";
     <?php
         if (!empty($_SESSION) && isset($_SESSION["pseudo"])){
             if (isset($_SESSION["statut"]) && $_SESSION["statut"] === "administrateur"){
+
+
+
                 genNavBar($_SESSION["statut"]);
-                echo "<h1>Welcome admin</h1>";
+                echo "<h1>Bienvenue admin</h1>";
+                echo "<section>\n<h2>Suprimez un objet</h2>\n</section>";
+                echo "<section>\n<h2>Tables</h2>\n</section>";
+                formSupression();
+
+                echo "<article>\n<h3>Les représentants</h3>\n";
+                affiche_tableau(get_table("repr"), ["Nom représentant", "Ville"]);
+                echo "</article>\n";
+
+                echo "<article>\n<h3>Les produits</h3>\n";
+                affiche_tableau(get_table("prod"), ["Nom produit", "Couleur", "Prix"]);
+                echo "</article>\n";
+
+                echo "<article>\n<h3>Les clients</h3>\n";
+                affiche_tableau(get_table("cli"), ["Nom client", "Ville"]);
+                echo "</article>\n";
+
+                echo "<article>\n<h3>Tout</h3>\n";
+                affiche_tableau(get_table(""), ["Nom représentant", "Ville représentant", "Nom client", "Ville client", "Nom produit", "Couleur", "Prix", "Quantité"]);
+                echo "</article>\n";
             }
             else{
                 echo '<script>window.location = "index.php"</script>';
