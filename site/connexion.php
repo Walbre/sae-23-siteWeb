5dbd77272ebe21d066751f46c3aaf785f657c810
OldSwade <133116353+OldSwade@users.noreply.github.com>
2023-06-18 10:00:13 +0200

    connexion.php final

--- site/connexion.php
+++ site/connexion.php
@@ -5,13 +5,19 @@ require "fonctions.php";
 ?>
 
 <!DOCTYPE html>
-<html lang="fr">
-    <head>
-		<meta charset="utf-8">
-		<title>connexion</title>
-	</head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Connexion</title>
+</head>
     <body>
 	<h2>Bienvenue sur la page connexion ! </h2>
+
+<article>	
 <?php
 if (!empty($_GET) && isset($_GET["action"]) && $_GET["action"]=="logout") {
 				session_destroy();
@@ -24,18 +30,6 @@ if (!empty($_POST) && isset($_POST['login']) && isset($_POST['pass']) && !(empty
 
 	$_SESSION["pseudo"] = $tab[0];
 	$_SESSION["statut"] = $tab[1];
-
-	/*if ( authentification($_POST["login"],$_POST["pass"]) )		{					
-		$_SESSION["login"]=$_POST["login"];
-			if ($_POST["login"]=="admin" && $_POST["pass"]=="Lannion!")  {
-				$_SESSION["statut"]="administrateur";
-				echo "<p>C'est bon</p>";
-			}
-			else {
-			$_SESSION["statut"]="utilisateur";
-			}
-	}
-*/
 
 
 
@@ -66,5 +60,11 @@ if (empty($_SESSION)){
 <?php
 }
 ?>	    
+</article>
 
-    </body>
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
